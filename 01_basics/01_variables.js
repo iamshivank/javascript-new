47226f215c417df7a647b5af10990056e603796c
Shivank <officialshivaank001@gmail.com>
2024-08-26 19:20:20 +0000

    New container changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 23907;
+let accountEmail = "shivank@capillarytech.com";
+var accountPassword = "January1995"; //never ever use var as variable, always use let.
+accountCity = "Ranchi";
+/*
+const and let is used for 
+*/
+let schoolName; // if you wont declare anything then value would be undefined.
+
+accountEmail = "shivank.company001@gmail.com";
+accountPassword = "shivank2211";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, schoolName]); //array is created to make a table using large brackets.
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+/*
+"useStrict"; // Treat the JS code as newer version of JS
+
+alert("Hello") // We are using nodeJS not browsers
+*/
+let name = "Shivank"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+// number -> 2 to the power 53
+// bigint -> used for big numbers
+// string -> ""
+// boolean -> true or false
+// null -> standalone value
+// undefined -> 
+// symbol -> uniqueness
+
+
+// object ->  
+
+console.log(typeof age); // typeof when null, type will be object of null. Whereas undefined is undefefined only.
